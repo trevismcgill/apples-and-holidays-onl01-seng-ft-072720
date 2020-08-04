c4aa4dbc8578a57e5ec7f7d36188cda2ebf0f5e2
Trevis McGill <trevismcgill25@gmail.com>
2020-08-04 03:13:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,23 +1,23 @@
 require 'pry'
 def second_supply_for_fourth_of_july(holiday_hash)
   given that holiday_hash looks like this:
-  holiday_hash = {
-    :winter => {
-      :christmas => ["Lights", "Wreath"],
-      :new_years => ["Party Hats"]
-    },
-    :summer => {
-      :fourth_of_july => ["Fireworks", "BBQ"]
-    },
-    :fall => {
-      :thanksgiving => ["Turkey"]
-    },
-    :spring => {
-      :memorial_day => ["BBQ"]
-    }
-  }
-
-  {memorial_day: BBQ}
+  # holiday_hash = {
+  #   :winter => {
+  #     :christmas => ["Lights", "Wreath"],
+  #     :new_years => ["Party Hats"]
+  #   },
+  #   :summer => {
+  #     :fourth_of_july => ["Fireworks", "BBQ"]
+  #   },
+  #   :fall => {
+  #     :thanksgiving => ["Turkey"]
+  #   },
+  #   :spring => {
+  #     :memorial_day => ["BBQ"]
+  #   }
+  # }
+  # 
+  # {memorial_day: BBQ}
   return the second element in the 4th of July array
   holiday_hash[:summer][:fourth_of_july][1]
 end
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, ooga)
 
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-  holiday_hash.each do {|season, holidays| season == :winter}
+  holiday_hash.each do |season, holidays|
     if season == :winter
       holidays.each do |holiday, supplies|
         supplies.push(ooga)
